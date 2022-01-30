44fecfcb715a1d6832fdf670c890caf90bd7e94e
kpettke <krzysiek.piotr@gmail.com>
2022-01-30 23:47:08 +0100

    fizzbuzz add

---
+++ FizzBuzz.py
@@ -0,0 +1,16 @@
+# FizzBuzz Python
+
+for i in range(1,101):
+    #FizzBuzz
+    if i%3==0 and i%5==0:
+        print("FizzBuzz")
+    #Fizz
+    elif i%3==0:
+        print("Fizz")
+
+    #Buzz
+    elif i%5==0:
+        print("Buzz")
+    #no
+    else:
+        print(i)
